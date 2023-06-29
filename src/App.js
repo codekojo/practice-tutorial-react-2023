8c56e4eb5e09aca4253e254d97de748309674883
CLINTON <42769351+codekojo@users.noreply.github.com>
2023-06-29 22:58:46 +0000

    Birthday Reminder Component
    First React Birthday Reminder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { faker } from "@faker-js/faker";
+
+function _calculateAge(birthday) {
+  // birthday is a date
+  var ageDifMs = Date.now() - birthday.getTime();
+  var ageDate = new Date(ageDifMs); // miliseconds from epoch
+  return Math.abs(ageDate.getUTCFullYear() - 1970);
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <article className="card__main">
+      <h2>5 birthdays today</h2>
+      <CardDetails />
+      <CardDetails />
+      <CardDetails />
+      <CardDetails />
+      <CardDetails />
+      <CardDetails />
+
+      <div className="btnClearAll" onClick={() => alert("Bow")}>
+        <h3>Clear All</h3>
+      </div>
+    </article>
   );
 }
 
 export default App;
+
+// Card Component
+
+function CardDetails() {
+  return (
+    <section className="card">
+      <img
+        src={faker.image.urlPicsumPhotos()}
+        alt="random avatar from faker"
+        className="avatar__image"
+      />
+      <div className="card__content">
+        <h6 className="card__username">{faker.person.fullName()}</h6>
+        <p>{_calculateAge(faker.date.birthdate())} years</p>
+      </div>
+    </section>
+  );
+}
